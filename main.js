347b36a06e0c01920d0791125437774662d43b7e
matvey202020 <matvey.tokarev.1020@mail.ru>
2025-12-09 17:37:41 +0300

    модифицировал сайт

---
+++ main.js
@@ -0,0 +1,9 @@
+let a = document.querySelector('.section_item2')
+a.addEventListener('click', function() {
+    location.assign('https://web.telegram.org/a/')
+})
+let b = document.querySelector('.github')
+b.addEventListener('click', function() {
+    location.assign('https://github.com/matvey202020')
+})
+
